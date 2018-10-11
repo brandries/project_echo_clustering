2011c52a19cf20c5add058b8c7972f9b905f396d
brandries <andriesvanderwalt@gmail.com>
2018-10-11 14:51:27 +0200

    finalize monte carlo

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,20 +1,9 @@
-
-# coding: utf-8
-
-# # Monte Carlo
-
-# In[1]:
-
+# Monte Carlo
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import metrics
-
-
-# In[53]:
-
-
 from matplotlib import rc
 font = {'family' : 'sans-serif',
         'size'   : 24}
@@ -22,44 +11,19 @@ font = {'family' : 'sans-serif',
 rc('font', **font)
 
 
-# In[2]:
-
-
-df_train = pd.read_csv('../../data/sampled_ts_train.csv', keep_default_na =False)
-
-
-# In[3]:
-
+df_train = pd.read_csv('../Group Project Stage/sampled_ts_train.csv', keep_default_na =False)
 
 df_train['tran_date'] = pd.to_datetime(df_train['tran_date'])
 df_train.set_index('tran_date', inplace=True)
 
 
-# In[4]:
-
-
-df_train.head()
-
-
-# In[5]:
-
-
 listof = [38767, 55991, 50962, 48676]
-
-
-# In[6]:
 
 
 subset_df = df_train[df_train['sku_key']==38767]['sales']
 
 
-# In[7]:
-
-
 subset_df.head()
-
-
-# In[8]:
 
 
 def monte_carlo(df, item):
@@ -93,63 +57,31 @@ def monte_carlo(df, item):
     return train, test, predictions
 
 
-# In[9]:
-
-
-true_freqs
-
-
-# In[10]:
-
-
-listof
-
-
-# In[136]:
-
-
 sku = 50962
-
-
-# In[137]:
 
 
 train, test, predictions = monte_carlo(df_train, sku)
 
 
-# In[138]:
-
-
-test.sum()
-
-
-# In[139]:
-
 
 pred_rmse = np.sqrt(metrics.mean_squared_error(test, predictions))
 pred_mse = metrics.mean_squared_error(test, predictions)
 pred_sales = int(predictions.sum().values)
-print('RMSE: %.3f' % rmse)
-print('MSE: %.3f' % mse)
+print('RMSE: %.3f' % pred_rmse)
+print('MSE: %.3f' % pred_mse)
 print('There are predicted to sell {} items'.format(pred_sales))
 
 true_rmse = np.sqrt(metrics.mean_squared_error(test.iloc[1:], test.iloc[:-1]))
 true_mse = metrics.mean_squared_error(test.iloc[1:], test.iloc[:-1])
 true_sales = int(test.sum())
-print('RMSE: %.3f' % rmse)
-print('MSE: %.3f' % mse)
+print('RMSE: %.3f' % true_rmse)
+print('MSE: %.3f' % true_mse)
 print('There are predicted to sell {} items'.format(true_sales))
-
-
-# In[140]:
 
 
 predictions.columns = ['Predictions']
 test = pd.DataFrame(test)
 test.columns = ['True']
-
-
-# In[141]:
 
 
 f, ax = plt.subplots(figsize=(15,9))
@@ -163,5 +95,5 @@ f.text(1,0.85,'Predictions Sales: {}'.format(round(pred_sales)))
 f.text(1,0.75,'True MSE: {}'.format(round(true_mse, 3)))
 f.text(1,0.7,'True Sales: {}'.format(round(true_sales)))
 
-f.savefig('./images/monte_carlo_{}.png'.format(sku), bbox_inches='tight')
+f.savefig('./monte_carlo_{}.png'.format(sku), bbox_inches='tight')
 
