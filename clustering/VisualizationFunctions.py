6b366cfbbff5a74035f49f8bef7c85977f5abd88
brandries <andriesvanderwalt@gmail.com>
2018-10-22 07:55:59 +0000

    work on viz script and functions

--- clustering/VisualizationFunctions.py
+++ clustering/VisualizationFunctions.py
@@ -1,46 +1,15 @@
-# Clustering of timeseries data
-import pandas as pd
-import numpy as np
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-from sklearn.manifold import TSNE
-from sklearn.cluster import AgglomerativeClustering
-from sklearn.preprocessing import MinMaxScaler
-
-print('Reading in the data...')
-agg = pd.read_csv('sku_labels.csv')
-df = pd.read_csv('extracted_features.csv')
-df.dropna(axis=1, inplace=True)
-
-scale = MinMaxScaler()
-skus = df['id']
-df.set_index('id', inplace=True)
-X = scale.fit_transform(df)
-names = df.columns
-
-print('Running Dimentionality Reduction...')
-dimred = TSNE(2)
-tsnes = dimred.fit_transform(X)
-
-#Merge tsne coordinates onto original df with sku_keys
-plot_df = pd.DataFrame(tsnes).join(df.reset_index())
-
-#Merge above tsne and features table to sku_key and categories
-plot_df['sku_key'] = plot_df['id'].astype(int)
-agg['sku_key'] = agg['sku_key'].astype(int)
-plot_df = plot_df.merge(agg, how='left', on='sku_key')
-
-colors=['b', 'r', 'g', 'y', 'm', 'orange', 'gold', 'skyblue']
+# Functions and classes for visualization
 
 def plot_by_factor(df, factor, colors, showplot=False):
     ''' Plot by factor on a already constructed
     t-SNE plot.
     '''
+    import matplotlib.pyplot as plt
+
     listof = {}     # this gets numbers to get the colors right
-    for i, j in enumerate(plot_df[factor].unique()):
+    for i, j in enumerate(df[factor].unique()):
         listof[j] = i
-    plot_df[factor] = plot_df[factor].map(listof)
+    df[factor] = df[factor].map(listof)
 
     f, ax = plt.subplots(figsize=(15,12))
     for i in df[factor].unique():
@@ -55,16 +24,11 @@ def plot_by_factor(df, factor, colors, showplot=False):
     else:
         f.savefig('images/{}.png'.format(factor))
 
-plot_by_factor(plot_df, 'sku_department', colors)
-
-
-#This is where the clusters come into play. here you have to read in
-#the files which contain clusters from the different methods and
-#assess them.
 
 class AnalyzeClusters(object):
     def __init__(self):
         pass
+
 
     def make_dataset(self, sales_df, clus_df):
         sales_df['sku_key'] = sales_df['sku_key'].astype(int)
@@ -78,7 +42,8 @@ class AnalyzeClusters(object):
         return self.c_dfs
 
 
-    def plot_cluster_continuous(self, cluster_dfs, categories, showplot=False):
+    def plot_cluster_continuous(self, cluster_dfs, categories, colors, showplot=False):
+        import matplotlib.pyplot as plt
         for j in categories:
             f, ax = plt.subplots(figsize=(15,10))
             for a, i in enumerate(cluster_dfs.keys()):
@@ -97,14 +62,16 @@ class AnalyzeClusters(object):
                 f.savefig('images/{}.png'.format('{}-{}'.format(i, j)))
 
     def plot_cluster_continuous_box(self, cluster_dfs, categories, showplot=False):
+        import matplotlib.pyplot as plt
+        import pandas as pd
         for j in categories:
             f, ax = plt.subplots(figsize=(15,10))
             for a, i in enumerate(cluster_dfs.keys()):
                 if a == 0:
-                    int_df = pd.DataFrame(df_dict[i][j])
+                    int_df = pd.DataFrame(cluster_dfs[i][j])
                     int_df.columns = [i]
                 else:
-                    temp = pd.DataFrame(df_dict[i][j])
+                    temp = pd.DataFrame(cluster_dfs[i][j])
                     temp.columns = [i]
                     int_df = int_df.join(temp)
                     int_df = int_df.fillna(0)
@@ -120,14 +87,16 @@ class AnalyzeClusters(object):
 
 
     def plot_cluster_continuous_violin(self, cluster_dfs, categories, showplot=False):
+        import matplotlib.pyplot as plt
+        import pandas as pd
         for j in categories:
             f, ax = plt.subplots(figsize=(15,10))
             for a, i in enumerate(cluster_dfs.keys()):
                 if a == 0:
-                    int_df = pd.DataFrame(df_dict[i][j])
+                    int_df = pd.DataFrame(cluster_dfs[i][j])
                     int_df.columns = [i]
                 else:
-                    temp = pd.DataFrame(df_dict[i][j])
+                    temp = pd.DataFrame(cluster_dfs[i][j])
                     temp.columns = [i]
                     int_df = int_df.join(temp)
                     int_df = int_df.fillna(0)
@@ -141,14 +110,16 @@ class AnalyzeClusters(object):
                 f.savefig('images/{}.png'.format('{}-{}'.format(i, j)))
 
     def plot_cluster_categorical(self, cluster_dfs, categories, showplot=False):
+        import matplotlib.pyplot as plt
+        import pandas as pd
         for j in categories:
             print('\n\n', j)
             for a, i in enumerate(cluster_dfs.keys()):
                 if a == 0:
-                    int_df = pd.DataFrame(df_dict[i][j].value_counts())
+                    int_df = pd.DataFrame(cluster_dfs[i][j].value_counts())
                     int_df.columns = [i]
                 else:
-                    temp = pd.DataFrame(df_dict[i][j].value_counts())
+                    temp = pd.DataFrame(cluster_dfs[i][j].value_counts())
                     temp.columns = [i]
                     int_df = int_df.join(temp)
                     int_df = int_df.fillna(0)
@@ -163,19 +134,3 @@ class AnalyzeClusters(object):
                 f.savefig('images/{}.png'.format('{}-{}'.format(i, j)))
 
         self.int_df = int_df
-
-colors = ['darkblue', 'tomato', 'orchid', 'darkorange', 'lime', 'gold', 'dodgerblue', 'pink',
-          'grey', 'darkgreen', 'y', 'slateblue', 'r', 'brown']
-run_cont = ['sales', 'selling_price', 'avg_discount']
-run_cats = ['sku_department', 'sku_subdepartment', 'sku_category', 'sku_subcategory']
-
-product_sales = pd.read_csv('aggregate_products.csv')
-clusters = pd.read_csv('som_clusters.csv')
-clusters.columns = ['sku_key', 'cluster']
-
-analyze = AnalyzeClusters()
-df_dict = analyze.make_dataset(product_sales, clusters)
-
-analyze.plot_cluster_continuous(df_dict, run_cont)
-analyze.plot_cluster_continuous_box(df_dict, run_cont)
-analyze.plot_cluster_categorical(df_dict, run_cats)
