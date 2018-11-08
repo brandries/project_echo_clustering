d1b16f72482b24ef16ec52c5900ed0664d1f9af7
brandries <andriesvanderwalt@gmail.com>
2018-11-08 13:49:13 +0200

    add visuals which compares rolling continuous features and number of products

--- clustering/VisualizationFunctions.py
+++ clustering/VisualizationFunctions.py
@@ -42,38 +42,45 @@ class AnalyzeClusters(object):
 
         return self.c_dfs
 
-    def plot_median_timeseries(self, cluster_dfs, split=False):
+    def plot_median_timeseries(self, cluster_dfs, variable='sales', split=False):
         import pandas as pd
         import matplotlib.pyplot as plt
         for i, j in cluster_dfs.items():
-            df = pd.DataFrame(j[['sales', 'tran_date']].groupby('tran_date').median())
+            df = pd.DataFrame(j[[variable, 'tran_date']].groupby('tran_date').median())
             df.set_index(pd.to_datetime(df.index), inplace=True)
             df.plot(figsize=(15,10))
             plt.title(i)
             if split == True:
                 plt.show()
 
-    def plot_mean_timeseries(self, cluster_dfs, split=False):
+    def plot_mean_timeseries(self, cluster_dfs, variable='sales', split=False):
         import pandas as pd
         import matplotlib.pyplot as plt
         for i, j in cluster_dfs.items():
-            df = pd.DataFrame(j[['sales', 'tran_date']].groupby('tran_date').mean())
+            df = pd.DataFrame(j[[variable, 'tran_date']].groupby('tran_date').mean())
             df.set_index(pd.to_datetime(df.index), inplace=True)
             df.plot(figsize=(15,10))
             plt.title(i)
             if split == True:
                 plt.show()
 
-    def plot_rolling_mean_timeseries(self, cluster_dfs, period=7, split=False):
+    def plot_rolling_mean_timeseries(self, cluster_dfs, variable='sales',
+                                     plot_second = False, period=7):
         import pandas as pd
         import matplotlib.pyplot as plt
         for i, j in cluster_dfs.items():
-            df = j[['sales', 'tran_date']].groupby('tran_date').mean().rolling(period).mean()
+            f, ax = plt.subplots(figsize=(15,10))
+            df = j[[variable, 'tran_date']].groupby('tran_date').mean().rolling(period).mean()
             df.set_index(pd.to_datetime(df.index), inplace=True)
-            df.plot(figsize=(15,10))
+            df.columns = ['Rolling {} level'.format(variable)]
+            df2 = j[[variable, 'tran_date']].groupby('tran_date').count()
+            df2.set_index(pd.to_datetime(df2.index), inplace=True)
+            df2.columns = ['Number of Products']
+            if plot_second == True:
+                df2.plot(kind='line', ax=ax)
+            df.plot(ax=ax, secondary_y=True, color='r')
             plt.title(i)
-            if split == True:
-                plt.show()
+            plt.show()
 
     def plot_nan_start(self, cluster_df):
         import pandas as pd
