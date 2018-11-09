500d53be0f8cc6dbcdc54e79d47717b084c756df
brandries <andriesvanderwalt@gmail.com>
2018-11-09 15:29:57 +0200

    new file structure

---
+++ src/models/dynamic_time_warping.py
@@ -0,0 +1,123 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, fcluster
+from dtaidistance import dtw, dtw_visualisation, clustering
+from dtaidistance import dtw_visualisation as dtwvis
+from sklearn.preprocessing import RobustScaler, StandardScaler
+import pickle
+from dimred_clustering import *
+run_plots = False
+
+class Preprocessing(object):
+    def __init__(self):
+        pass
+
+    def pivot_table(self, df):
+        print('Manipulate and pivot table...')
+        df['sku_key'] = df['sku_key'].astype(int)
+        df.drop(['sku_department', 'sku_subdepartment',
+                 'sku_category', 'sku_subcategory', 'sku_label'],
+                axis=1, inplace=True)
+        product_ts = pd.pivot_table(df, values='sales',
+                                    index='sku_key', columns='tran_date')
+        return product_ts
+
+    def sort_nas(self, df):
+        df['nas'] = df.apply(lambda x: x.isna()).sum(axis=1)
+        print('There are {} products with less than 50% entries'\
+        .format(len(df[df['nas'] > len(df.columns)/2])))
+        self.df_ordered = df.sort_values('nas', ascending=True).drop('nas', axis=1)
+        return self.df_ordered
+
+    def split_nans(self, pivot_table, feature_df):
+        sorted_df = pivot_table.copy()
+        sorted_df['nan'] = sorted_df.iloc[:,0].apply(np.isnan)
+        pivot_no_nans = sorted_df[sorted_df['nan'] == False].drop('nan', axis=1)
+        pivot_nans = sorted_df[sorted_df['nan'] == True].drop('nan', axis=1)
+        nans = feature_df.loc[list(pivot_nans.index),:]
+        no_nans = feature_df.loc[list(pivot_no_nans.index),:]
+        return pivot_nans, nans, pivot_no_nans, no_nans
+
+    def plot_nas(self, df):
+        plt.figure(figsize=(5,10))
+        plt.imshow(df, cmap='hot', interpolation='nearest')
+        plt.show()
+
+    def make_diff_length_list(self, df):
+        self.product_ts_fill = df.fillna(0)
+        self.product_matrix_fill = self.product_ts_fill.values
+        self.product_matrix = df.values
+        product_dict = {}
+        product_list = []
+        for i, j in zip(range(len(self.product_matrix)), df.index):
+            product_dict[j] = self.product_matrix[i][~np.isnan(self.product_matrix[i])]
+            product_list.append(self.product_matrix[i][~np.isnan(self.product_matrix[i])])
+
+        self.product_dict = product_dict
+        self.product_list = product_list
+
+
+class DynamicTimeWarping(object):
+    def __init__(self):
+        pass
+
+    def distance_matrix(self, df):
+        print('Producing distance matrix...')
+        ds = dtw.distance_matrix_fast(df)
+        if run_plots == True:
+            f, ax = dtw_visualisation.plot_matrix(ds)
+            f.set_size_inches(12, 12)
+        return ds
+
+    def linkage_tree(self, df):
+        print('Producing linkage Tree')
+        self.model = clustering.LinkageTree(dtw.distance_matrix_fast, {})
+        clusters_dtw = self.model.fit(df)
+        return clusters_dtw
+        pickle.dump(self.model, open('model.pkl', 'wb'))
+        if run_plots == True:
+            f, ax = self.model.plot()
+            f.set_size_inches(17, 20)
+
+    def cluster(self, model, cluster_nr):
+        threshold = cluster_nr
+        clusters = fcluster(model.linkage, threshold,
+                            criterion='inconsistent', depth=10)
+        return clusters
+
+        if run_plots == True:
+            fig = plt.figure(figsize=(20, 20))
+            dendrogram(model.linkage, orientation='left', leaf_font_size=15,
+                       color_threshold=100, labels=product_ts.index[:subsample])
+            plt.show()
+
+
+
+
+def main():
+    dp = DataPreprocess()
+    labels, df = dp.read_data('sku_labels.csv', 'extracted_features.csv')
+    product_sales = pd.read_csv('aggregate_products.csv')
+    scaler = StandardScaler()
+    X = dp.scale_data(df, scaler)
+    names = df.columns
+
+    pp = Preprocessing()
+    product_ts = pp.pivot_table(product_sales)
+    product_ts = pp.sort_nas(product_ts)
+    product_ts.to_csv('pivot_products.csv')
+    if run_plots == True:
+        pp.plot_nas(product_ts)
+    pp.make_diff_length_list(product_ts)
+    dtw = DynamicTimeWarping()
+    clusters_dtw = dtw.linkage_tree(pp.product_matrix_fill)
+    clusters = dtw.cluster(dtw.model, 6)
+    dtw_df = product_ts.reset_index()
+    dtw_df['cluster'] = clusters
+    output_df = dtw_df[['sku_key', 'cluster']]
+    print('Outputting...')
+    output_df.to_csv('dtw_clusters.csv', index=False)
+
+if __name__ == '__main__':
+    main()
---
+++ src/models/selforganizing_maps.py
@@ -0,0 +1,120 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+import sompy
+from sompy.sompy import SOMFactory
+from sompy.visualization.mapview import View2D
+from sompy.visualization.bmuhits import BmuHitsView
+from sompy.visualization.hitmap import HitMapView
+from dimred_clustering import DataPreprocess
+from sklearn.preprocessing import RobustScaler, StandardScaler
+from dynamic_time_warping import Preprocessing
+
+def knn_elbow(df, k_range=20, plot=False):
+    from sklearn.cluster import KMeans
+    scores = {}
+    for i in range(1, k_range+1):
+        kmeans = KMeans(n_clusters=i)
+        clusters_fit = kmeans.fit_predict(df)
+        scores[i] = kmeans.inertia_
+    if plot == True:
+        pd.DataFrame(scores, index=['score']).T.plot(figsize=(15,8))
+        plt.title('Elbow KMeans')
+        plt.xlabel('K')
+        plt.show()
+    return scores
+
+def plot_figures(som):
+    #BMU map
+    vhts  = BmuHitsView(12,12,"Hits Map",text_size=12)
+    #U matrix
+    u = sompy.umatrix.UMatrixView(50, 50, 'umatrix', show_axis=True,
+                                  text_size=8, show_text=True)
+    UMAT  = u.build_u_matrix(sm, distance=1, row_normalized=False)
+    #Cluster map
+    sm.cluster(6)
+    hits  = HitMapView(10,10,"Clustering",text_size=12)
+    #Show factor influence
+    view2D  = View2D(15,15,"time-series",text_size=10, names=names)
+    #Show plots
+    view2D.show(sm, col_sz=4, which_dim="all", denormalize=True)
+    vhts.show(sm, anotate=True, onlyzeros=False, labelsize=12,
+              cmap="Greys", logaritmic=False)
+    UMAT = u.show(sm, distance2=1, row_normalized=False, show_data=True,
+                  contooor=True, blob=False)
+    a = hits.show(sm)
+    plt.show()
+
+def make_class_dict(clusters):
+    map_dict = {}
+    for i, j in enumerate(clusters):
+        map_dict[i] = j
+    return map_dict
+
+def assign_from_som(clusters, model):
+    map_dict = make_class_dict(clusters)
+    assignment = pd.DataFrame(model._bmu).T
+    assignment[0] = assignment[0].astype(int)
+    cluster_assignments = assignment[0].map(map_dict)
+    df_assigned = pd.DataFrame(cluster_assignments)
+    return df_assigned
+
+
+class BuildSOM(object):
+    def __init__(self):
+        pass
+
+    def build_som(self, X):
+        print('Building SOM...')
+        sm = SOMFactory().build(X, normalization = 'var',
+                                mapsize=(15,15), initialization='pca')
+        sm.train(n_job=1, verbose='info',
+                 train_rough_len=100, train_finetune_len=200)
+
+        topographic_error = sm.calculate_topographic_error()
+        quantization_error = np.mean(sm._bmu[1])
+        print ("Topographic error = {}; Quantization error = {}"\
+        .format(topographic_error,quantization_error))
+        return sm
+
+
+def main():
+    show_plots = False
+    subset = 'none'
+    df = pd.read_csv('extracted_features.csv')
+    df.set_index('id', inplace=True)
+    df.dropna(axis=1, inplace=True)
+    pp = Preprocessing()
+    feat = pd.read_csv('aggregate_products.csv')
+    pivot = pp.pivot_table(feat)
+    sorted = pp.sort_nas(pivot)
+    pivot_nans, nans, pivot_no_nans, no_nans = pp.split_nans(sorted, df)
+    scaler = StandardScaler()
+    if subset == 'nan':
+        use_df = nans
+    elif subset == 'no_nans':
+        use_df = no_nans
+    elif subset == 'none':
+        use_df = df
+    print('There are {} samples'.format(len(use_df)))
+    X = scaler.fit_transform(use_df)
+    som = BuildSOM()
+    model = som.build_som(X)
+    if show_plots == True:
+        plot_figures(sm)
+    print('Getting optimal K-clusters...')
+    nclus = 6
+    clusters = model.cluster(n_clusters=nclus)
+    df_assigned = assign_from_som(clusters, model)
+    df_assigned.index = use_df.index
+    print('Outputting...')
+    df_assigned.to_csv('som_clusters.csv')
+
+if __name__ == '__main__':
+    main()
+
+# Find a way to implement this to do hyperparameter search automatically
+#som_scores = knn_elbow(model._normalizer.denormalize_by(model.data_raw,
+#                                                        model.codebook.matrix),
+#                                                        40)
