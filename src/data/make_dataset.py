ab40cdeb7d242d3290572106a276abaee60522f5
brandries <andriesvanderwalt@gmail.com>
2018-11-12 09:13:46 +0200

    make notebook a function

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,70 @@
 # -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from tsfresh import extract_features
+from dynamic_time_warping import Preprocessing
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+def main():
+    df = pd.read_csv('complete_df_7.csv')
+    if df.columns[0] == 'Unnamed: 0':
+        df.drop('Unnamed: 0', axis=1, inplace=True)
+    df['stock_open'] = df['stock_open'].astype(float)
+    #aggregate to the product level across stores
+    aggregate = df.groupby(['sku_key', 'tran_date'])\
+    .agg({'sales':'sum', 'selling_price':'mean',
+          'avg_discount': 'mean', 'stock_open': 'sum'})
+    aggregate.reset_index(inplace=True)
 
+    #Get the categorical variables for each product
+    categorical = df[['sku_key', 'sku_department', 'sku_subdepartment',
+                  'sku_category', 'sku_subcategory', 'sku_label']]
+    nw_df = pd.DataFrame([], columns=['sku_key','sku_department',
+                                      'sku_subdepartment','sku_category',
+                                      'sku_subcategory', 'sku_label'])
+    for i in categorical['sku_key'].unique():
+        nw_df = pd.concat([nw_df, pd.DataFrame(categorical[categorical['sku_key'] == i].iloc[0]).T])
+    nw_df.reset_index(inplace=True, drop=True)
+
+    #Join aggregate product to the categorical variables
+    aggregate['sku_key'] = aggregate['sku_key'].astype(int)
+    nw_df['sku_key'] = nw_df['sku_key'].astype(int)
+    aggregate_df = aggregate.merge(nw_df, how='left', on='sku_key')
+    aggregate_df.to_csv('aggregate_products.csv', index=False)
+
+    #Extract features from the time series using tsfresh
+    aggregate_df['tran_date'] = pd.to_datetime(df['tran_date'])
+    extracted_features = extract_features(aggregate_df[['sku_key', 'tran_date',
+                                                        'sales']],
+                                          column_id="sku_key",
+                                          column_sort="tran_date")
+    extracted_features.to_csv('extracted_features.csv')
+
+    #Extract the categorical features for each SKU and save
+    categorical = aggregate_df[['sku_key', 'sku_department',
+                                'sku_subdepartment', 'sku_category',
+                                'sku_subcategory', 'sku_label']]
+    nw_df = pd.DataFrame([], columns=['sku_key', 'sku_department',
+                                      'sku_subdepartment', 'sku_category',
+                                      'sku_subcategory', 'sku_label'])
+    for i in categorical['sku_key'].unique():
+        nw_df = pd.concat([nw_df, pd.DataFrame(categorical[categorical['sku_key'] == i].iloc[0]).T])
+    nw_df.reset_index(inplace=True, drop=True)
+    nw_df.to_csv('sku_labels.csv', index=False)
+
+    #Pivot Table and save dfs with and without nans
+    pp = Preprocessing()
+    pivot_table = pp.pivot_table(aggregate_df)
+    sorted_df = pp.sort_nas(pivot_table)
+    pp.make_diff_length_list(sorted_df)
+    pivot_nans, nans, pivot_no_nans, no_nans = pp.split_nans(pivot_table, extracted_features)
+
+    pivot_table.to_csv('pivot_table.csv')
+    pivot_nans.to_csv('nans_pivot_table.csv')
+    pivot_no_nans.to_csv('no_nans_pivot_table.csv')
+    nans.to_csv('extracted_feat_nans.csv')
+    no_nans.to_csv('extracted_feat_no_nans.csv')
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
     main()
